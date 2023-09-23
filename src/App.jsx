01b7460763d4af9f89c018b315ea8bedb436ae39
Way2SelfAware <neo@DESKTOP-DDPIL1P>
2023-09-23 12:55:40 -0700

    cartContext is unaffected. Removed reprganizeItems filter/sort works! Need to style

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ import Navbar from "./components/navbar/navbar";
 import Shop from "./pages/shop/shop";
 import Cart from "./pages/cart/cart";
 import ProductDetailsPage from "./components/products/ProductDetailsPage";
-import CategoryPage from "./components/reorganizeItems/CategoryPage";
-import SortPage from "./components/reorganizeItems/sortPage";
 import Login from "./pages/user/login";
 import "./App.css";
 import { CartProvider } from "./pages/cart/cartContext";
@@ -18,8 +16,6 @@ function App() {
           <Navbar />
           <Routes>
             <Route path="/" element={<Shop />} />
-            <Route path="/category/:categoryName" element={<CategoryPage />} />
-            <Route path="/sort/:sortName" element={<SortPage />} />
             <Route path="/product/:id" element={<ProductDetailsPage />} />
             <Route path="/cart" element={<Cart />} />
             <Route path="/login" element={<Login />} />
